0c19af3bbfa7053f8d2b7ba6cdf679b6d57e094b
silkfinik <silkfink@gmail.com>
2025-10-16 01:49:08 +0300

    chore: Set up initial project structure

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "EchoLog"
+include(":app")
+include(":domain")
+include(":core:common")
+include(":core:ui")
+include(":data")
+include(":feature:map")
+include(":feature:echo_creation")
+include(":feature:echo_detail")
+include(":feature:journals")
